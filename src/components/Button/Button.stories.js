4f629b4f3840931bf2625faadc8cc7dbb52e734d
Lenarqa <lenar.valiev211@gmail.com>
2024-06-15 18:52:47 +0700

    add folder, add story names

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "./Button";
 
 export default {
-  title: "MyButton",
+  title: "Form/MyButton",
   component: Button,
 };
 
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Input } from "./Input";
 
 export default {
-  title: "MyInput",
+  title: "Form/MyInput",
   component: Input,
 };
 
 export const InputSizeS = () => <Input size="s" placeholder="Small size" />;
 export const InputSizeMD = () => <Input size="md" placeholder="Medium size" />;
 export const InputSizeX = () => <Input size="x" placeholder="Big size" />;
+
+InputSizeS.storyName = "Small Input";
+InputSizeMD.storyName = "Medium Input";
+InputSizeX.storyName = "Big Input";
